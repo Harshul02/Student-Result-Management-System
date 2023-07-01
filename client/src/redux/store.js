a243a3842f07f1b8e6c763ab6a7b1e17bec4a6a8
Harshul Jain <harshulgangwal02@gmail.com>
2023-07-01 18:23:45 +0530

    Added employee redux and protected Route

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,11 @@
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { alertSlice } from "./alerts";
-// import { employeeSlice } from "./employees";
+import { employeeSlice } from "./employees";
 
 const rootReducer = combineReducers({
-  alert: alertSlice.reducer
-//   employee: employeeSlice.reducer,
+  alert: alertSlice.reducer,
+  employee: employeeSlice.reducer,
 });
 
 const store = configureStore({
