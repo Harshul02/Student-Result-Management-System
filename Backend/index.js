785cc30bfbcd026637340a76c9f78d15f529841f
Harshul Jain <harshulgangwal02@gmail.com>
2023-07-12 15:14:50 +0530

    Added CORS

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 5000 | process.env.PORT;
+var cors = require('cors');
 
 require("dotenv").config();
 const dbConfig = require("./config/db");
 app.use(express.json());
+app.use(cors());
 
 const employeeRoute = require("./routes/employeeRoute");
 const studentRoute = require("./routes/studentRoute");
